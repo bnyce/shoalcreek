77267c40887fbecc27a26cf1a2ddeaa19287699f
Bryce Benton <brycebenton+pantheon2@gmail.com>
2023-11-01 14:47:30 -0500

    export nov1

--- web/sites/default/files/ahc/supplemental/previous/script.sh
+++
@@ -1,2 +0,0 @@
-cd /var/www/files/cms/ahc/supplemental/previous/
-sudo bash -c 'ls -I index.html -I index-html.sh -I script.sh | ./index-html.sh > index.html'
--- web/sites/default/files/ahc/supplemental/script.sh
+++
@@ -1,2 +0,0 @@
-cd /var/www/files/cms/ahc/supplemental/
-sudo bash -c 'ls -I b -I previous -I index.html -I index-html.sh -I script.sh | ./index-html.sh > index.html'
